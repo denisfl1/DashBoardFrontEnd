7f7ebffcdc8ef2095649027e02c9d80296b1c184
denisfl1 <denisfl1@hotmail.com>
2024-01-01 16:20:41 -0300

    update01

--- src/components/LatateralBar.tsx
+++ src/components/LatateralBar.tsx
@@ -3,6 +3,7 @@ import logout from '../Icons/sair.png'
 import config from '../Icons/configuracoes.png'
 import grafic from '../Icons/grafico.png'
 import home from '../Icons/home.png'
+import categories_icon from '../Icons/categories.png'
 
 const LateralBar:React.FC=()=>{
 
@@ -18,7 +19,7 @@ return(
                 <div className="LateralBarList"><img src={home}></img><li>Home</li></div>
                 <div className="LateralBarList"><img src={grafic}></img><li>Dashboard</li></div>
                 <div className="LateralBarList"><img ></img><li>Products</li></div>
-                <div className="LateralBarList"><img ></img><li>Categories</li></div>
+                <div className="LateralBarList"><img src={categories_icon }></img><li>Categories</li></div>
                 <div className="LateralBarList"><img src={config}></img><li>Settings</li></div>
                 <div className="LateralBarList"><img src={logout}></img><li>Sair</li></div>
 
