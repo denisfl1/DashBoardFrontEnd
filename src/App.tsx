6aa14f9a5d0fea44a47fc770ed2020c7e3376b0b
denisfl1 <denisfl1@hotmail.com>
2024-01-03 19:30:01 -0300

    newFeature2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import LateralBar from './components/LatateralBar'
 import HomePage from './Homepage';
-import Header from './components/Header';
+
 
 function App() {
   return (
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from '../Icons/logo.png'
+import{ useParams} from 'react-router-dom'
+
 
 
 const Login:React.FC=()=>{
+    const {param} = useParams()
+    const [RorL ,setRorL] = useState(false)
+    
 
+    const HandleRegister:React.MouseEventHandler<HTMLButtonElement> = (e)=>{
+        e.preventDefault()
+        console.log(param)
+        setRorL(true)
 
-
+    }
 
 
     return( 
 
         <div className="LoginContainer">
 
-                <div className="LoginContent">
+                {!RorL ?<div className="LoginContent">
                   
                     <form>
                     <img src={logo}></img>
@@ -21,15 +30,42 @@ const Login:React.FC=()=>{
                     <label>Senha</label>
                     <input></input>
 
-                    <button>LOGIN</button>
+                    <button>Login</button>
                     
                     <div className="HRContainer">
-                        <div className="HrLeft"><hr></hr></div><div className="ORDiv">OR</div><div className="HrRight"><hr></hr></div>
+                        <div className="HrLeft"><hr></hr></div><div className="OR_Div">OU</div><div className="HrRight"><hr></hr></div>
                     </div>
-                    <button>REGISTRE-SE</button>
+                    <button onClick={HandleRegister}>Registre-se</button>
+                    </form>
+
+                </div>:
+                <>
+                <div className="RegisterIMG"> <img style={{width:'400px',height:'400px'}} src={logo}></img></div>
+                <div className="RegisterContent">
+                  
+                    <form>
+             
+                    <label>Nome</label>
+                    <input type="text" name="name"></input>
+                    <label>Sobrenome</label>
+                    <input type="text" name="surname"></input>
+                    <label>E-mail</label>
+                    <input type="email" name="email"></input>
+                 
+                    <label>Senha</label>
+                    <input type="password" name="password"></input>
+
+                    <label>Repita sua Senha</label>
+                    <input  type="password" name="password"></input>
+
+                    <button style={{marginTop:'20px'}}>Registrar</button>
+    
+    
                     </form>
 
                 </div>
+                </>
+                }
 
 
         </div>
